259c0d71f9265a857d9886499d4af1dee796fa2d
epeuva <epeuva@gmail.com>
2018-08-02 16:09:06 +0200

    Added a webView to load github

--- project4-easy-browser/project4-easy-browser/ViewController.swift
+++ project4-easy-browser/project4-easy-browser/ViewController.swift
@@ -7,12 +7,35 @@
 //
 
 import UIKit
+import WebKit // for WKWebView
 
-class ViewController: UIViewController {
+
+// Check page 195 (book) for important info about Struts + Classes
+// ViewController inherits from UIViewController & implements WKNavigationDelegate protocol
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        
+        // Delegation
+        // Tell the webview that when something happens, inform the controller
+        // You need to conform to the protocol if WKWebView
+        webView.navigationDelegate = self
+        
+        view = webView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Create an URL object from an url
+        let url = URL(string: "https://github.com/epeuva")!
+        
+        // WKWebView only loads urls from URLRequest
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
     }
 
     override func didReceiveMemoryWarning() {
