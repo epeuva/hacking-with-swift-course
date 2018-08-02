cc07d510c3af2cca4fefdac0a4aa380d73aa395a
epeuva <epeuva@gmail.com>
2018-08-02 16:27:47 +0200

    Added open right bar button item in order to be able to open 3 predefined websites

--- project4-easy-browser/project4-easy-browser/ViewController.swift
+++ project4-easy-browser/project4-easy-browser/ViewController.swift
@@ -30,6 +30,8 @@ class ViewController: UIViewController, WKNavigationDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
         // Create an URL object from an url
         let url = URL(string: "https://github.com/epeuva")!
         
@@ -37,12 +39,31 @@ class ViewController: UIViewController, WKNavigationDelegate {
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @objc func openTapped() {
+        // actionSheet type of alert with empty message (only title)
+        let ac = UIAlertController(title: "Open page...", message:nil, preferredStyle: .actionSheet)
+        
+        ac.addAction(UIAlertAction(title: "apple.com",style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "github.com/epeuva", style: .default, handler: openPage))
+        
+        // Empty cancel button in order to close the actionSheet
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        // Only for iPad devices (where action sheet should be anchored)
+        ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+        present(ac, animated: true)
     }
-
-
+    
+    func openPage(action: UIAlertAction) {
+        let url = URL(string: "https://" + action.title!)!
+        webView.load(URLRequest(url: url))
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
+    
 }
 
