afa5309cbf9a5d82f4a770f7f93f28a6a5ab0bde
epeuva <epeuva@gmail.com>
2018-08-02 11:42:49 +0200

    Added loadCounties method. Added initial askQuestion method, loading the first three county flags to buttons

--- project2-Guess-the-flag/project2-Guess-the-flag/ViewController.swift
+++ project2-Guess-the-flag/project2-Guess-the-flag/ViewController.swift
@@ -14,14 +14,54 @@ class ViewController: UIViewController {
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button3: UIButton!
     
+    /* Defining variables using  "type inference" */
+    // Array of countries
+    var countries = [String]()
+    
+    // Game Score
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        loadCountries()
+        
+        askQuestion()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func askQuestion() {
+        // .normal  refers to "the normal value of UIControlState."
+        // See Page 165 (book) for more info.
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+    }
+    
+    /// Loads the default counties for the game
+    func loadCountries(){
+        //Method 1:
+        /*countries.append("estonia")
+        countries.append("france")
+        countries.append("germany")
+        countries.append("ireland")
+        countries.append("italy")
+        countries.append("monaco")
+        countries.append("nigeria")
+        countries.append("poland")
+        countries.append("russia")
+        countries.append("spain")
+        countries.append("uk")
+        countries.append("us")*/
+        
+        // Method 2:
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+ 
     }
     
     
