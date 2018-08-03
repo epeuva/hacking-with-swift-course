71103b8795c99c6e9775bb52ef4b4855cd34aa71
epeuva <epeuva@gmail.com>
2018-08-03 09:56:49 +0200

    Embebbed all into a Navigation Controller. Loaded the words of the start.txt file. Use the first randomized one as a title.

--- project5-world-scramble/project5-world-scramble/ViewController.swift
+++ project5-world-scramble/project5-world-scramble/ViewController.swift
@@ -7,19 +7,42 @@
 //
 
 import UIKit
+import GameplayKit
 
 class ViewController: UITableViewController {
+    
+    // All the words from the star.txt file
+    var allWords = [String]()
+    
+    // Words used in game
+    var usedWords = [String]()
+    
+    // Default words
+    var defaultWords = ["silkworm"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // Try to load the words of the start.txt file
+        if let startWordsPath = Bundle.main.path(forResource: "start", ofType: "txt") {
+            if let startWords = try? String(contentsOfFile: startWordsPath) {
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        } else {
+            allWords = defaultWords
+        }
+        
+        print(allWords)
+        
+        startGame()
     }
-
+    
+    func startGame() {
+        allWords = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: allWords) as! [String]
+        title = allWords[0]
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+    }
 
 }
 
