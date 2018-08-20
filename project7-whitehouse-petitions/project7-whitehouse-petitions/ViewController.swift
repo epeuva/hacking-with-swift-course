6dedc2ad427b62dfcdf1e35bf9b0c9bf6257a1ca
epeuva <epeuva@gmail.com>
2018-08-20 15:56:17 +0200

    Added tableView methods (numberOfRows and cellForRowAt)

--- project7-whitehouse-petitions/project7-whitehouse-petitions/ViewController.swift
+++ project7-whitehouse-petitions/project7-whitehouse-petitions/ViewController.swift
@@ -9,17 +9,26 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    
+    var petitions = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count;
     }
-
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = "Title goes here"
+        cell.detailTextLabel?.text = "Subtitle goes here"
+        
+        return cell
+    }
 
 }
 
