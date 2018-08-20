885968b2c006cae1bd966427f035f6194f0c8411
epeuva <epeuva@gmail.com>
2018-08-20 12:46:18 +0200

    Added sizeToFit() calls in order to fix the issue of labels not showing on the default view

--- project6b-auto-layout-in-code/project6b-auto-layout-in-code/ViewController.swift
+++ project6b-auto-layout-in-code/project6b-auto-layout-in-code/ViewController.swift
@@ -23,26 +23,31 @@ class ViewController: UIViewController {
         label1.translatesAutoresizingMaskIntoConstraints = false // Disables iOS Auto Layout constraints
         label1.backgroundColor = UIColor.red
         label1.text = "THESE"
+        label1.sizeToFit() // Labels does not show without this call
         
         let label2 = UILabel()
         label2.translatesAutoresizingMaskIntoConstraints = false // Disables iOS Auto Layout constraints
         label2.backgroundColor = UIColor.cyan
         label2.text = "ARE"
+        label2.sizeToFit() // Labels does not show without this call
         
         let label3 = UILabel()
         label3.translatesAutoresizingMaskIntoConstraints = false // Disables iOS Auto Layout constraints
         label3.backgroundColor = UIColor.yellow
         label3.text = "SOME"
+        label3.sizeToFit() // Labels does not show without this call
         
         let label4 = UILabel()
         label4.translatesAutoresizingMaskIntoConstraints = false // Disables iOS Auto Layout constraints
         label4.backgroundColor = UIColor.green
         label4.text = "AWESOME"
+        label4.sizeToFit() // Labels does not show without this call
         
         let label5 = UILabel()
         label5.translatesAutoresizingMaskIntoConstraints = false // Disables iOS Auto Layout constraints
         label5.backgroundColor = UIColor.orange
         label5.text = "LABELS"
+        label5.sizeToFit() // Labels does not show without this call
         
         // Add labels to the view
         view.addSubview(label1)
