ff115145f33cdc81be09d34779a517e1d677a0e9
epeuva <epeuva@gmail.com>
2018-08-20 12:39:58 +0200

    Added 5 labels to the main view

--- project6b-auto-layout-in-code/project6b-auto-layout-in-code/ViewController.swift
+++ project6b-auto-layout-in-code/project6b-auto-layout-in-code/ViewController.swift
@@ -9,17 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    /// Do not show the status bar on this view controller. Caution!!: This overrides a property of UIViewController, not a method!
+    //override var prefersStatusBarHidden: Bool {
+    //    return true
+    //}
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Create five UILabel
+        let label1 = UILabel()
+        label1.translatesAutoresizingMaskIntoConstraints = false // Disables iOS Auto Layout constraints
+        label1.backgroundColor = UIColor.red
+        label1.text = "THESE"
+        
+        let label2 = UILabel()
+        label2.translatesAutoresizingMaskIntoConstraints = false // Disables iOS Auto Layout constraints
+        label2.backgroundColor = UIColor.cyan
+        label2.text = "ARE"
+        
+        let label3 = UILabel()
+        label3.translatesAutoresizingMaskIntoConstraints = false // Disables iOS Auto Layout constraints
+        label3.backgroundColor = UIColor.yellow
+        label3.text = "SOME"
+        
+        let label4 = UILabel()
+        label4.translatesAutoresizingMaskIntoConstraints = false // Disables iOS Auto Layout constraints
+        label4.backgroundColor = UIColor.green
+        label4.text = "AWESOME"
+        
+        let label5 = UILabel()
+        label5.translatesAutoresizingMaskIntoConstraints = false // Disables iOS Auto Layout constraints
+        label5.backgroundColor = UIColor.orange
+        label5.text = "LABELS"
+        
+        // Add labels to the view
+        view.addSubview(label1)
+        view.addSubview(label2)
+        view.addSubview(label3)
+        view.addSubview(label4)
+        view.addSubview(label5)
+        
+        print("Checkpoint 1")
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
+    
+    
 
 }
 
