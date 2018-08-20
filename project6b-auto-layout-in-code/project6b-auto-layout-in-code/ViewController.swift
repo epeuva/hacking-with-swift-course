62989dcb67dbd671d2d54e7736bd180ad9e48cd2
epeuva <epeuva@gmail.com>
2018-08-20 13:29:12 +0200

    Added Auto Layout option 2 using Anchors

--- project6b-auto-layout-in-code/project6b-auto-layout-in-code/ViewController.swift
+++ project6b-auto-layout-in-code/project6b-auto-layout-in-code/ViewController.swift
@@ -56,6 +56,8 @@ class ViewController: UIViewController {
         view.addSubview(label4)
         view.addSubview(label5)
         
+        /* Auto Layout Option 1: Using VFL */
+        /*
         // Dictionary of views
         let viewsDictionary = ["label1": label1, "label2": label2,
                                "label3": label3, "label4": label4, "label5": label5]
@@ -77,13 +79,25 @@ class ViewController: UIViewController {
             // @999     Priority from 1 o 1000 (1000 equals to must have)
             view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", options: [], metrics: metrics, views: viewsDictionary))
         }
+        */
         
-        print("Checkpoint 1")
+        /* Auto Layout Option 2: Using Anchors */
+        var previous: UILabel?
+        
+        for label in [label1, label2, label3, label4, label5] {
+            label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+            label.heightAnchor.constraint(equalToConstant: 88).isActive = true
+            
+            if let previous = previous {
+                // we have a previous label – create a height constraint
+                label.topAnchor.constraint(equalTo:
+                    previous.bottomAnchor, constant: 10).isActive = true
+            }
+            
+            // set the previous label to be the current one, for the next loop iteration
+            previous = label
+        }
         
     }
-    
-    
-    
 
 }
-
