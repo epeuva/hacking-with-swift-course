6e7739448769581f04dc176c65868612af996562
epeuva <epeuva@gmail.com>
2018-08-21 13:26:34 +0200

    Added loadLevel method. Added events to buttons (using code).

--- project8-seven-swifty-words/project8-seven-swifty-words/ViewController.swift
+++ project8-seven-swifty-words/project8-seven-swifty-words/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GameplayKit
 
 class ViewController: UIViewController {
 
@@ -19,15 +20,73 @@ class ViewController: UIViewController {
     @IBOutlet weak var submitTapped: UIButton!
     @IBOutlet weak var clearTapped: UIButton!
     
+    var letterButtons = [UIButton]()
+    var activatedButtons = [UIButton]()
+    var solutions = [String]()
+    
+    var score = 0
+    var level = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // That's why we add 1001 tag to every button... in order to retrieve all UIButtons and be able to attach a method to the click event.
+        for subview in view.subviews where subview.tag == 1001 {
+            let btn = subview as! UIButton
+            letterButtons.append(btn)
+            btn.addTarget(self, action: #selector(letterTapped),for: .touchUpInside)
+        }
+        
+        loadLevel()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    /// Used in UIButton tab #selector to retrieve the button tapped
+    ///
+    /// - Parameter btn: the button tapped
+    @objc func letterTapped(btn: UIButton) {
+        print(btn)
+    }
+    
+    func loadLevel() {
+        var clueString = ""
+        var solutionString = ""
+        var letterBits = [String]()
+        
+        if let levelFilePath = Bundle.main.path(forResource: "level\(level)", ofType: "txt") {
+            if let levelContents = try? String(contentsOfFile: levelFilePath) {
+                var lines = levelContents.components(separatedBy: "\n")
+                lines = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: lines) as! [String]
+                
+                // .enumerated() used in order to know the position of each item.
+                for (index, line) in lines.enumerated() {
+                    let parts = line.components(separatedBy: ": ")
+                    let answer = parts[0]
+                    let clue = parts[1]
+                    clueString += "\(index + 1). \(clue)\n"
+                    let solutionWord = answer.replacingOccurrences(of: "|", with: "")
+                    solutionString += "\(solutionWord.count) letters\n"
+                    solutions.append(solutionWord)
+                    let bits = answer.components(separatedBy: "|")
+                    letterBits += bits
+                }
+            }
+        }
+        
+        // Remove new lines and spaces
+        cluesLabel.text = clueString.trimmingCharacters(in: .whitespacesAndNewlines)
+        answersLabel.text = solutionString.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        
+        letterBits = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: letterBits) as! [String]
+        if letterBits.count == letterButtons.count {
+            
+            // "..<" half-open range operator: do not include the upper limit
+            for i in 0 ..< letterBits.count {
+                letterButtons[i].setTitle(letterBits[i], for: .normal)
+            }
+        }
+
     }
 
 
