b45c3a1f3d0a21fea7b85f83430cc55c7e2a3cb8
epeuva <epeuva@gmail.com>
2018-08-22 17:34:43 +0200

    Added imagePickerController method having the code that is called when the user selects an image with the Add button

--- project10-names-to-faces/project10-names-to-faces/ViewController.swift
+++ project10-names-to-faces/project10-names-to-faces/ViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class ViewController: UICollectionViewController {
+// Class supports two protocols: UIImagePickerControllerDelegate, UINavigationControllerDelegate
+class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +21,10 @@ class ViewController: UICollectionViewController {
     
     /// Adds a person to the application
     @objc func addNewPerson() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true // Allows the user to edit (crop, ...) the photo
+        picker.delegate = self
+        present(picker, animated: true)
     }
     
     
@@ -28,11 +33,37 @@ class ViewController: UICollectionViewController {
         return 10
     }
     
+    
     // Number of items in the UICollectionView (placeholder at this moment)
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         // Create a view cell based on the created UICollectionViewCell Person
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as! PersonCell // Typecast to PersonCell
         return cell
+    }
+
+    
+    // Function called when the user has selected an image
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        // Ensure that we get an UIImage as a parameter
+        guard let image = info[UIImagePickerControllerEditedImage] as? UIImage else { return }
+        
+        // Generates a random UUID: Universally Unique Identifier
+        let imageName = UUID().uuidString
+        
+        // Gets the documents directory (private app folder) and appends the image name to it
+        let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
+        
+        // Tries to convert an UIImage to a Data object in order to be able to save it to the disk
+        if let jpegData = UIImageJPEGRepresentation(image, 80) {
+            try? jpegData.write(to: imagePath)
+        }
+        
+        dismiss(animated: true)
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        return URL(fileURLWithPath: "")
     }
 
     
