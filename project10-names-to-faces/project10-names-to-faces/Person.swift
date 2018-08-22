8e3b16074acf0f92148d04005fe3d7e5181b606b
epeuva <epeuva@gmail.com>
2018-08-22 17:58:23 +0200

    Added Person class, based on NSObject with 2 properties (name and image)

---
+++ project10-names-to-faces/project10-names-to-faces/Person.swift
@@ -0,0 +1,16 @@
+//
+//  Person.swift
+//  project10-names-to-faces
+//
+//  Created by epeuva on 22/08/2018
+//  Copyright © 2018 epeuva. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    
+    var name: String
+    var image: String
+
+}
