4588b1cfee7542e4204a6851095273c822693841
epeuva <epeuva@gmail.com>
2018-08-22 16:50:50 +0200

    Created PersonCell (UICollectionViewCell) and linked its fields to collection view cell

---
+++ project10-names-to-faces/project10-names-to-faces/PersonCell.swift
@@ -0,0 +1,18 @@
+//
+//  PersonCell.swift
+//  project10-names-to-faces
+//
+//  Created by epeuva on 22/08/2018
+//  Copyright © 2018 epeuva. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+    
+    
+}
