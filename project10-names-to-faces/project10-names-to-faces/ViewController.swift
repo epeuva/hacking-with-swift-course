1768647ed5f879ce57662e4f76c0315d498a4a44
epeuva <epeuva@gmail.com>
2018-08-22 13:08:28 +0200

    Init project10

---
+++ project10-names-to-faces/project10-names-to-faces/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  
+//
+//  Created by epeuva on 22/08/2018
+//  Copyright © 2018 epeuva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
