bfba88bafdf4ee521820b67e9fa875cd0cfab1ae
epeuva <epeuva@gmail.com>
2018-08-23 15:54:13 +0200

    Added IBOutlet for UIImageView and UISlider, and IBAction for UIButton and UISlider

--- project13-instafilter/project13-instafilter/ViewController.swift
+++ project13-instafilter/project13-instafilter/ViewController.swift
@@ -9,17 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var intensity: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    
+    @IBAction func save(_ sender: Any) {
     }
 
-
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
+    
 }
-
