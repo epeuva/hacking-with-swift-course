120bfb398c560e67225a138d5fee17ca6036ea71
epeuva <epeuva@gmail.com>
2018-08-24 11:22:57 +0200

    Added IBOutlet for UIButton. Added penguin at the center of the storyboard. Added a WiP tapped function

--- project15-animation/project15-animation/ViewController.swift
+++ project15-animation/project15-animation/ViewController.swift
@@ -10,16 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tap: UIButton!
+    
+    var imageView: UIImageView!
+    var currentAnimation = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Loads the resource "penguin" as an iname
+        imageView = UIImageView(image: UIImage(named: "penguin"))
+        
+        // Centers the penguin
+        imageView.center = CGPoint(x: 512, y: 384)
+        
+        // Adds the penguin image to the imageView
+        view.addSubview(imageView)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func tapped(_ sender: Any) {
+        
+        // Hide the tap button
+        tap.isHidden = true
+        
+        // Animate with 1 second, without delay
+        UIView.animate(withDuration: 1, delay: 0, options: [], animations:
+            { [unowned self] in // avoid strong references
+                switch self.currentAnimation {
+                case 0:
+                    break
+                default:
+                    break
+                }
+        }) { [unowned self] (finished: Bool) in // 
+            
+            // Show the tap button
+            self.tap.isHidden = false
+        }
+
+        currentAnimation += 1
+        
+        if currentAnimation > 7 {
+            currentAnimation = 0
+        }
     }
-
-
 }
-
